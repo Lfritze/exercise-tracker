15ba819014331c64d99af30542c76d76a68e4b23
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-04-13 22:30:59 -0400

    setup to deploy with scripts

--- mern-excercise-tracker/backend/server.js
+++ mern-excercise-tracker/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -12,6 +13,13 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
+
+if(process.env.NODE_ENV=== 'production') {
+  app.use(express.static('mern-exercise-tracker'));
+  agg.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'mern-exercise-tracker', 'build', 'index.html'));
+  });
+}
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
